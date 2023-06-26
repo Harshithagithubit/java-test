33db5f9d9b28185741835b7cb89ec5199f2c2bb2
akshay siddapelly <harshithasiddapelly@gmail.com>
2023-06-26 16:14:38 +0530

    adding test6.java

---
+++ src/Test6.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+			int a=4;
+			int b=6;
+			int operator=3;
+			if(operator ==1)
+			{
+				System.out.println(a+b);
+			}else if(operator ==2)
+			{
+				System.out.println(a-b);
+			}else if(operator ==3)
+			{
+				System.out.println(a*b);
+			}else if(operator ==4)
+			{
+				System.out.println(a/b);
+			}else
+			{
+				System.out.println(a%b);
+			}
+		}
+	}
+
+
