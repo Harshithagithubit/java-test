3578b8207b38ac0c291d5d9ae66437a337bf7b76
akshay siddapelly <harshithasiddapelly@gmail.com>
2023-06-26 16:31:45 +0530

    adding test7-java

---
+++ src/Test7.java
@@ -0,0 +1,14 @@
+
+public class Test7 {
+
+	public static void main(String[] args) {
+	 int x=4;
+	 int y=7;
+	 if(x>y) {
+		 System.out.println(x);
+	 }
+	 else {
+		 System.out.println(y); 
+	 }
+	}
+}
