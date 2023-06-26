6f8c880cb1bce00e23d3a35a8508e765e12bfe16
akshay siddapelly <harshithasiddapelly@gmail.com>
2023-06-26 18:08:55 +0530

    adding test9.java

---
+++ src/Test9.java
@@ -0,0 +1,14 @@
+
+public class Test9 {
+
+	public static void main(String[] args) {
+		  int i,n=7;
+		  int sum=0;
+		  for (i=1;i<=7;i++)
+		  {
+			  sum=sum+i;
+			  System.out.println(sum);
+		  }
+	}
+
+}
