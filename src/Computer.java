f31902c97f001494642b35fa01ad2bbdc56a0b21
akshay siddapelly <harshithasiddapelly@gmail.com>
2023-07-02 12:33:34 +0530

    adding computer.java

---
+++ src/Computer.java
@@ -0,0 +1,11 @@
+
+public class Computer {
+
+	public static void main(String[] args) {
+		Calc obj = new Calc();
+		int r1 = obj.add(4, 5);
+		int r2 = obj.sub(7, 3);
+		System.out.println(r1+" "+r2);
+	
+	}
+}
