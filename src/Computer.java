177af738c6b817480a48adc9910d9ec4821111a1
akshay siddapelly <harshithasiddapelly@gmail.com>
2023-07-02 17:59:52 +0530

    adding computer.java

--- src/Computer.java
+++ src/Computer.java
@@ -2,10 +2,12 @@
 public class Computer {
 
 	public static void main(String[] args) {
-		Calc obj = new Calc();
-		int r1 = obj.add(4, 5);
-		int r2 = obj.sub(7, 3);
-		System.out.println(r1+" "+r2);
-	
+		// TODO Auto-generated method stub
+		  Calc obj = new Calc();
+		  int r1= obj.add(4,5);
+		  int r2 = obj.sub(8,2);
+		  System.out.println(r1 + " "+ r2);
+		  
 	}
+
 }
