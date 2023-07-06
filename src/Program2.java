2729f9e028b15e58d246ef6580533a8bb8751252
akshay siddapelly <harshithasiddapelly@gmail.com>
2023-07-06 16:16:06 +0530

    adding program2.java

---
+++ src/Program2.java
@@ -0,0 +1,13 @@
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("navin");
+		System.out.println(sb.capacity());
+		sb.append("rao");
+		System.out.println(sb);
+		
+
+	}
+
+}
