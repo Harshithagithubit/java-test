7e81898402b6f0130d70155e8234d8f094a45ecc
akshay siddapelly <harshithasiddapelly@gmail.com>
2023-08-02 17:53:19 +0530

    adding list.java

---
+++ src/List.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class List {
+
+	public static void main(String[] args) {
+		
+		int[] list = {3,7,10,14,16};
+		
+	
+		for(int i=0;i<list.length;i++) {
+			
+		
+		if(list[i]>5) {
+			System.out.println(list[i]+"is greater than 5");
+		}
+		else  {
+			System.out.println(list[i]+"is lesser than 5");
+		}
+		
+		}
+	}
+	
+  }
+
+ 
